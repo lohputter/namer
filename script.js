55281c9528b6ed5b8d813b4a2f057bbad53cda5f
lohputter <129501406+lohputter@users.noreply.github.com>
2023-05-05 17:26:42 +0800

    445

---
+++ script.js
@@ -0,0 +1,43 @@
+function randomGenerate() {
+    let name = document.getElementById("name").value;
+    let length = document.getElementsByTagName("span").length;
+    let animations = ["square", "wave", "rainbow", "snake", "circle"];
+    let animation = animations[Math.floor(Math.random() * animations.length)];
+    let cooldowns = [1, 2, 2, 2, 2];
+    let cooldown = cooldowns[animations.indexOf(animation)];
+    for (let i=0; i<length; i++) {
+        document.getElementsByTagName("span")[0].remove();
+    }
+    if (document.getElementsByTagName("span").length == 0) {
+        for (let i=0; i<name.length; i++) {
+            setTimeout(function() {
+                let letter = document.createElement("span");
+                document.getElementById("styled").appendChild(letter);
+                letter.innerHTML = name[i];
+                letter.style.display = "block";
+                if (animation == "wave") {
+                    letter.style.animation = `0.5s ${animation} linear infinite`;
+                } else {
+                    letter.style.animation = `${cooldown}s ${animation} linear infinite`;
+                }
+                if (animation == "wave" || animation == "rainbow") {
+                    letter.style.left = `${i * (100/name.length)}%`;
+                }
+            }, i * (cooldown/name.length) * 1000);
+        }
+    }
+}
+function pause() {
+    let length = document.getElementsByTagName("span").length;
+    for (let i=0; i<length; i++) {
+        document.getElementsByTagName("span")[i].style.animation = "none";
+        document.getElementsByTagName("span")[i].style.display = "none";
+    }
+}
+function mode(n) {
+    if (n == 1) {
+        document.body.id = "light";
+    } else {
+        document.body.id = "";
+    }
+}
